74c78dc05a598a1a840d2f99bc6442629080d782
Jojo Aquino <jfaquinojr@gmail.com>
2016-09-21 00:59:33 +0800

    popup started

--- app/main.js
+++ app/main.js
@@ -10,7 +10,7 @@
 		vm.ticketsVisible = [];
 
 		function loadTickets() {
-			let url = "http://localhost:8001/api/Tickets/Open";
+			var url = "http://localhost:8001/api/Tickets/Open";
 			svc.get(url).then(function (result) {
 				vm.ticketsAll = vm.ticketsAll.concat(result.data);
 				vm.ticketsVisible = vm.ticketsAll;
@@ -18,22 +18,28 @@
 		}
 
 		function loadNewTicketswithinPastMinutes(minutes) {
-			let url = "http://localhost:8001/api/Tickets/Open/Past/Minutes/" + minutes;
+			var url = "http://localhost:8001/api/Tickets/Open/Past/Minutes/" + minutes;
 			svc.get(url).then(function (result) {
 				vm.ticketsAll = vm.ticketsAll.concat(result.data); //TODO: filter first <---!!!!!
 				vm.ticketsVisible = vm.ticketsAll;
 			});
 		}
 
-		function filterByPage(pageNo) {
-			vm.ticketsVisible = _.filter(vm.ticketsAll, function (ticket) {
-				return 
-			});
-		}
+		//function filterByPage(pageNo) {
+		//	vm.ticketsVisible = _.filter(vm.ticketsAll, function (ticket) {
+		//		return 
+		//	});
+		//}
 
-		vm.filterByPage = filterByPage;
+		//vm.filterByPage = filterByPage;
 
 		loadTickets();
 
-	})
+		vm.editTicket = function(ticketId) {
+			var dialog = $("#dialog").data("dialog");
+			dialog.open();
+		};
+
+
+	});
 })();
